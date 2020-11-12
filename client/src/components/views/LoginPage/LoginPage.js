63b6cdcd31949afb9405711ce73697d87a7d3d7b
comforme0 <comforme0@naver.com>
2020-11-13 00:38:06 +0900

    login

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useDispatch } from "react-redux";
+import { loginUser} from '../../../_actions/user_action'
 
-function LoginPage() {
+function LoginPage(props) {
+    const dispatch = useDispatch();
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswrodHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        let dataToSubmit = {
+            email: Email,
+            password: Password
+        }
+
+        dispatch(loginUser(dataToSubmit))
+            .then(response => {
+              if (response.payload.loginSuccess) {
+                props.history.push("/");
+              } else {
+                //setFormErrorMessage('Check out your Account or Password again')
+              }
+            })            
+        
+    }
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100', height: '100vh'
+        }}>
+            <form style={{ display: 'flex', flexDirection: 'column'}}
+                onSubmit={onSubmitHandler}>
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswrodHandler} />
+                <br />
+                <button type="submit">
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
